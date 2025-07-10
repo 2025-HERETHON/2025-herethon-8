528373389bd45e0538cfba6cca5f085113c47846
Nongsuh <20231350@sungshin.ac.kr>
2025-07-10 12:39:10 +0900

    feat: 지도 이동 기능

--- static/js/home.js
+++ static/js/home.js
@@ -5,25 +5,34 @@ var options ={
     };
 
 var map = new kakao.maps.Map(container, options);
+var geocoder = new kakao.maps.services.Geocoder();
+const search_button = document.getElementById('search_button');
 
 //주소검색 시 지도 이동
-function searchLocation(){
-    const search_address=document.getElementById('search_address').ariaValueMax;
-    const geocoder = new kakao.maps.services.Geocoder();
+search_button.addEventListener('click',()=>{
+    const search_address = document.getElementById('search_address');
+    const input_value=search_address.value;
 
-    geocoder.addressSearch(search_address,function(result,status){
-        if (status === kakao.maps.services.Status.OK) {
-            const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-            map.setCenter(coords);
+    if(input_value ===''){
+            alert('주소를 입력하세요')
+            return;
         }
-        else{
-            alert('주소를 찾을 수 없습니다.')
-        }
-    });
-}
+        geocoder.addressSearch(input_value,function(result,status){
+            if (status === kakao.maps.services.Status.OK) {
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+                map.setCenter(coords);
+            }
+            else{
+                alert('주소를 찾을 수 없습니다.')
+            }
+        });
+
+        loadCriminalMarkers(input_value);
+});
+    
+
 
 // 성범죄자 주소 띄우기 (검색어 한 개로 처리)
-
 function loadCriminalMarkers(query) {
     if (!query || query.trim() === '') {
         alert('검색어를 입력해주세요.');
@@ -64,7 +73,6 @@ function loadCriminalMarkers(query) {
                         });
 
                         if (isFirst) {
-                            map.setCenter(coords);
                             isFirst = false;
                         }
                     } else {
@@ -79,8 +87,3 @@ function loadCriminalMarkers(query) {
         });
 }
 
-// 검색 버튼 클릭 시 입력값을 받아서 loadCriminalMarkers 호출하기
-document.getElementById('search_button').addEventListener('click', function() {
-    const searchInput = document.getElementById('search_address').value;
-    loadCriminalMarkers(searchInput);
-});
